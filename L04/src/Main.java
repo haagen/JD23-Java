a34a0cc6714da15503894d6c41aa9655a181d0f1
Martin Haagen <martin@mbh.se>
2023-10-24 10:48:28 +0200

    1/2 av lektion 4

---
+++ L04/src/Main.java
@@ -0,0 +1,107 @@
+import java.util.*; // Importera allt i paketet
+
+public class Main {
+    public static void main(String[] args) {
+
+            //javaArray();
+            //javaArrayList();
+            javaLinkedList();
+
+    }
+
+    public static void javaArray() {
+
+        /*
+            En array en lista bestående av lika datatyper.
+
+            De definieras med att lägga [] efter datatypens namn.
+
+            Man kan initiera den genom att skriva värdena komma-
+            separerat ino {}-parenteser.
+
+         */
+
+        int[] lottoRad = { 2, 7, 9, 12, 23, 30, 31 };
+
+        boolean giltigLottoRad = true;
+        for(int i=0; i<lottoRad.length; i++) {
+            if(lottoRad[i] < 0 || lottoRad[i] > 31) {
+                giltigLottoRad = false;
+                break;  // När vi hittar ett ogiltigt tal vet vi att hela raden är ogiltig. Vi behöver därför inte fortsätta att loopa.
+            }
+        }
+
+        if(giltigLottoRad){
+            System.out.println("Lottoraden är giltig!");
+        } else {
+            System.out.println("Lottoraden är inte giltig!");
+        }
+
+        // Hur lägger man då till element i listan? Eller
+        // tar bort ett element?
+
+        // Man måste skapa en ny lista. Vi lägger till en extra
+        // plats i vår array.
+        int[] newArray = new int[lottoRad.length + 1 ];
+        for(int i=0; i< lottoRad.length; i++) {
+            newArray[i] = lottoRad[i];
+        }
+        newArray[ lottoRad.length ] = 5;
+        lottoRad = newArray;
+    }
+
+    public static void javaArrayList() {
+
+        // Skapa en ny tom lista
+        ArrayList<String> bilar = new ArrayList<String>();
+
+        bilar.add("Suzuki");        // Element 0
+        bilar.add("Volvo");         // Element 1
+        bilar.add("Land Rover");    // Element 2
+        bilar.add("Lada");          // Element 3
+
+        // Skriva ut lista till skärmen
+        System.out.println(bilar);
+
+        // Skriv ett specifikt element .get(<number>)
+        System.out.printf("Det bästa bilmärket är så klart %s\n", bilar.get(0));
+
+        // Ändrar ett specifikt värde med .set(<number>, <new value>)
+        bilar.set(3, "Fiat");
+        System.out.println(bilar);
+
+        // Ta bort ett specifikt element med .remove(<number>)
+        bilar.remove(3);
+        System.out.println(bilar);
+
+        System.out.println("For-loop: ");
+        for(int i=bilar.size()-1; i>=0; i--) {
+            System.out.println(bilar.get(i));
+        }
+
+        System.out.println("En annan for-loop: ");
+        for(String bil : bilar) {
+            System.out.println(bil);
+        }
+
+        // Vi kan tömma listan med .clear();
+        bilar.clear();
+        System.out.println("Nu innehåller listan: " + bilar);
+    }
+
+    public static void javaLinkedList() {
+        // Skapa en ny tom lista
+        LinkedList<String> top10MetalBands = new LinkedList<String>();
+        top10MetalBands.add("Metallica");
+        top10MetalBands.addLast("Iron Maiden");
+        top10MetalBands.addFirst("Rammstein");
+        System.out.println(top10MetalBands);
+
+        System.out.printf(
+                "Det mest omtyckta metall bandet i JD23 är %s och det minst omtyckta är %s\n",
+                top10MetalBands.getFirst(),
+                top10MetalBands.getLast()
+        );
+    }
+
+}
